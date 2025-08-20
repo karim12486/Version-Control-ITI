d6767597925ffafdc431e189f62301d867c80543
Karim Reda <karim.reda.69420@gmail.com>
2025-08-20 12:03:15 +0300

    added a new function to HelloJava.js

--- HelloJava.js
+++ HelloJava.js
@@ -2,6 +2,10 @@ class HelloJava {
     printHelloWorld() {
         console.log("Hello World!");
     }
+
+    printHelloCS(){
+        console.log("Hello CS students");
+    }
 }
 
 module.exports = HelloJava;
